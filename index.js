3a746c6fc8bc6b77d59aaa4bf88eb893378241f5
Travis Clarkson <tk1667@gmail.com>
2018-02-06 02:17:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,11 +10,24 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+ var price = Math.floor(Math.random() * 100)
+ var obj = {[item]: price}
+ console.log(`${item} has been added to your cart.`)
+ cart.push(obj)
+ return cart
 }
 
 function viewCart() {
-  // write your code here
+  var cart = getCart()
+  if(cart.length === 0) {
+    console.log("Your shopping cart is empty.")
+  } else {
+    string = "In your cart, you have "
+    for( const prop in cart) {
+      string = string + `${prop} at $${cart[prop]}.`
+    }
+    console.log(string)
+  }
 }
 
 function total() {
