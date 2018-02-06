458684ea7d9c4baf2ab585c09531fec51c771680
Travis Clarkson <tk1667@gmail.com>
2018-02-06 04:25:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -67,6 +67,8 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  console.log(cardNumber)
+  if (cardNumber === undefined) {
+    console.log("Sorry, we don't have a credit card on file for you.")
+  }
 }
 placeOrder()
